4928bfe074ec39ce72c27bb9078b8ed50672e938
Nicolas Bevacqua <nicolasbevacqua@gmail.com>
2015-10-30 19:33:00 -0300

    Added ServiceWorker support

--- js/global.js
+++ js/global.js
@@ -18,4 +18,16 @@
 
   });
 
+  // ServiceWorker is a progressive technology. Ignore unsupported browsers
+  if('serviceWorker' in navigator) {
+    console.log('CLIENT: service worker registration in progress.');
+    navigator.serviceWorker.register('/service-worker.js').then(function() {
+      console.log('CLIENT: service worker registration complete.');
+    }, function() {
+      console.log('CLIENT: service worker registration failure.');
+    });
+  } else {
+    console.log('CLIENT: service worker is not supported.');
+  }
+
 })();
