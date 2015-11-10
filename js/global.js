f0c74ae632d9a80d7ea1f2140ad0acd1040a8269
Chris Coyier <chriscoyier@gmail.com>
2015-11-09 19:04:01 -0600

    styleguide

--- js/global.js
+++ js/global.js
@@ -19,7 +19,7 @@
   });
 
   // ServiceWorker is a progressive technology. Ignore unsupported browsers
-  if('serviceWorker' in navigator) {
+  if ('serviceWorker' in navigator) {
     console.log('CLIENT: service worker registration in progress.');
     navigator.serviceWorker.register('/service-worker.js').then(function() {
       console.log('CLIENT: service worker registration complete.');
